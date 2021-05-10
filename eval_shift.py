e291cd03dae807b6b501e7a2fed34d4d218bdc81
Ben Kompa <ben.kompa@gmail.com>
2021-05-10 14:47:57 -0400

    indent

--- eval_shift.py
+++ eval_shift.py
@@ -131,25 +131,25 @@ def eval_on_dataset_shift(dict_args):
 
 
 def main(): 
-parser = argparse.ArgumentParser("Eval model on shifted data")
+	parser = argparse.ArgumentParser("Eval model on shifted data")
 
-parser.add_argument("--data_dir", default="data/cifar10/")
-parser.add_argument("--model_dir", default='/mnt/medqaresourcegroupdiag/medqa-fileshare/users/bk117/models')
-parser.add_argument("--model_name", required=True)
-parser.add_argument("--classifier", type=str, default="resnet18_RBF")
-parser.add_argument("--split", type=str, choices=['roll', 'rot', 'cifar10_c', 'val'])
-#parser.add_argument("--layer", type=int, default=4)
-parser.add_argument("--pre_activation", action="store_true")
+	parser.add_argument("--data_dir", default="data/cifar10/")
+	parser.add_argument("--model_dir", default='/mnt/medqaresourcegroupdiag/medqa-fileshare/users/bk117/models')
+	parser.add_argument("--model_name", required=True)
+	parser.add_argument("--classifier", type=str, default="resnet18_RBF")
+	parser.add_argument("--split", type=str, choices=['roll', 'rot', 'cifar10_c', 'val'])
+	#parser.add_argument("--layer", type=int, default=4)
+	parser.add_argument("--pre_activation", action="store_true")
 
-parser.add_argument("--precision", type=int, default=32, choices=[16, 32])
-parser.add_argument("--batch_size", type=int, default=256)
-parser.add_argument("--max_epochs", type=int, default=100)
-parser.add_argument("--num_workers", type=int, default=8)
-parser.add_argument("--val_split", type=int, default=4, choices=[0,1,2,3,4])
-parser.add_argument("--gpu_id", type=str, default="0")
+	parser.add_argument("--precision", type=int, default=32, choices=[16, 32])
+	parser.add_argument("--batch_size", type=int, default=256)
+	parser.add_argument("--max_epochs", type=int, default=100)
+	parser.add_argument("--num_workers", type=int, default=8)
+	parser.add_argument("--val_split", type=int, default=4, choices=[0,1,2,3,4])
+	parser.add_argument("--gpu_id", type=str, default="0")
 
-parser.add_argument("--learning_rate", type=float, default=1e-2)
-parser.add_argument("--weight_decay", type=float, default=1e-2)
+	parser.add_argument("--learning_rate", type=float, default=1e-2)
+	parser.add_argument("--weight_decay", type=float, default=1e-2)
 
 	args = parser.parse_args()
 	dict_args = vars(args) 
